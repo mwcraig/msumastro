09196e3163674fbb7a5477cd1f8bdc0b1c01fc71
Matt Craig <mcraig@mnstate.edu>
2011-10-02 19:56:13 -0500

    Moved names of keywords and comments to a single file.

---
+++ keyword_names.sh
@@ -0,0 +1,12 @@
+latitude_name="LATITUDE"
+latitude_comment="[degrees] Observatory latitude"
+longitude_name="LONGITUD"
+longitude_comment="[deg west] Observatory longitude"
+
+lst_name="LST"
+lst_comment="Local Sidereal Time"
+jd_name="JD"
+jd_comment="Julian Date at start of observation"
+mjd_name="MJD"
+mjd_comment="Modified Julian Date MJD=JD-2400000.5 at start of
+observation"
