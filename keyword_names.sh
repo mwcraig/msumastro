db9f8944fda91ce907b265d9ab952b802cb6af30
Matt Craig <mcraig@mnstate.edu>
2011-10-02 22:50:08 -0500

    Script to add ra/dec to file from object name or to convert maxim ra/dec to standard.

--- keyword_names.sh
+++ keyword_names.sh
@@ -1,8 +1,12 @@
+# list of keyword names and comments included in each processing routine
+
+# latitude/longitude
 latitude_name="LATITUDE"
 latitude_comment="[degrees] Observatory latitude"
 longitude_name="LONGITUD"
 longitude_comment="[deg west] Observatory longitude"
 
+# time keywords
 lst_name="LST"
 lst_comment="Local Sidereal Time"
 jd_name="JD"
@@ -10,3 +14,11 @@ jd_comment="Julian Date at start of observation"
 mjd_name="MJD"
 mjd_comment="Modified Julian Date MJD=JD-2400000.5 at start of
 observation"
+
+# RA/DEC
+ra_name="RA"
+ra_comment="Approximate RA at EQUINOX"
+dec_name="DEC"
+dec_comment="Approximate DEC at EQUINOX"
+equinox_name="EQUINOX"
+equinox_comment="Equniox of RA and DEC"
