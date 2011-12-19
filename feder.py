7df64b654cad032264ed5a44df16baf5906e4737
Matt Craig <mcraig@mnstate.edu>
2011-12-18 20:25:56 -0600

    Added feder object.

---
+++ feder.py
@@ -0,0 +1,6 @@
+from astropysics import obstools
+
+feder = obstools.Site(lat=46.86678,
+                      long=-96.453278,
+                      alt=311.8,
+                      name='Feder Observatory')
