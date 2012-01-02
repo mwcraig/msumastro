6804b83729579af2d4f90e5b8a3eaa3ba503a6e8
Matt Craig <mcraig@mnstate.edu>
2012-01-01 22:17:37 -0600

    Added documentation, improved handling of case when solve fails.

--- astrometry.py
+++ astrometry.py
@@ -2,12 +2,29 @@ import subprocess
 from os import path, remove, rename
 
 def call_astrometry(filename, sextractor=False, feder_settings=True,
-                   no_plots=True, minimal_output=True,
-                   ra_dec=None):
+                    no_plots=True, minimal_output=True,
+                    ra_dec=None, overwrite=False):
+    
     """Wrapper around astrometry.net solve-field.
-
-    Provides several groups of options that work well with images from
-    the Feder telescope.
+    
+    :param sextractor:
+        True to use `sextractor`, or a string with the
+        path to sextractor.
+    :param feder_settings:
+        Set True if you want to use plate scale appropriate for Feder
+        Observatory Apogee Alta U9 camera.
+    :param no_plots:
+        True to suppress astrometry.net generation of
+        plots (pngs showing object location and more)
+    :param minimal_output:
+        Suppress, as separate files, output of: WCS
+        header, RA/Dec object list, matching objects list.
+    :param ra_dec:
+        List or tuple of RA and Dec; also limits search
+        radius to 1 degree.
+    :param overwrite:
+        If True, write the WCS header to the input FITS
+        file.
     """
     solve_field = ["solve-field"]
     option_list = []
@@ -29,7 +46,10 @@ def call_astrometry(filename, sextractor=False, feder_settings=True,
 
     if ra_dec is not None:
         option_list.append("--ra %s --dec %s --radius 1" % ra_dec)
-
+        
+    if overwrite:
+        option_list.append("--overwrite")
+        
     options = " ".join(option_list)
 
     solve_field.extend(options.split())
@@ -40,7 +60,13 @@ def call_astrometry(filename, sextractor=False, feder_settings=True,
 def add_astrometry(filename, overwrite=False, ra_dec=None):
     """Add WCS headers to FITS file using astrometry.net
 
-    Boolean, returns True on success.
+    `overwrite` should be `True` to overwrite the original file. If `False`,
+    the file astrometry.net generates is kept.
+
+    `ra_dec` is a list or tuple (RA, Dec) in either decimal or
+    sexagesimal form.
+
+    Returns `True` on success.
     
     Tries a couple strategies before giving up: first sextractor,
     then, if that fails, astrometry.net's built-in soure extractor.
@@ -48,32 +74,31 @@ def add_astrometry(filename, overwrite=False, ra_dec=None):
     It also cleans up after astrometry.net, keeping only the new FITS
     file it generates and the .solved file.
 
-    overwrite should be True to overwrite the original file. If False,
-    the file astrometry.net generates is kept.
-
-    ra_dec is a list or tuple (RA, Dec) in either decimal or
-    sexagesimal form.
+    For more flexible invocation of astrometry.net, see :func:`call_astrometry`
     """
+    base, ext = path.splitext(filename)
+
     solved_field = (call_astrometry(filename,
-                                    sextractor='/opt/local/bin/sex',ra_dec=ra_dec)
+                                    sextractor='/opt/local/bin/sex',
+                                    ra_dec=ra_dec)
                     == 0)
 
     if not solved_field:
-            solved_field = (call_astrometry(filename ,ra_dec=ra_dec)
+            solved_field = (call_astrometry(filename, ra_dec=ra_dec,
+                                            overwrite=True)
                             == 0)
 
-    if not solved_field:
-        return False
-
-    # we solved, time to clean up:
-    base, ext = path.splitext(filename)
-    if overwrite:
+    if overwrite and solved_field:
         try:
             rename(base+'.new', filename)
         except OSError:
             return False
+
+    # whether we succeeded or failed, clean up
     remove(base+'.axy')
-    remove(base+'-indx.xyls')
-    return True
+    if solved_field:
+        remove(base+'-indx.xyls')
+        
+    return solved_field
     
         
