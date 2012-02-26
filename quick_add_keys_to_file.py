4a4939f7b4ba3e365d4eff7f19966ae8696ae19f
Matt Craig <mcraig@mnstate.edu>
2012-02-26 15:24:10 -0600

    Initial commit of kludgy way to add keyword/value pairs to a list of fits files.

---
+++ quick_add_keys_to_file.py
@@ -0,0 +1,30 @@
+import asciitable as at
+import pyfits
+
+def add_keys(file_list, keys=keylist):
+    """Add keywords to a list of FITS files.
+
+    `file_list` should have one file per line.
+
+    `keylist` can be in any format easily readable by
+    asciitable. There needs to be a header line followed by, on each
+    line, a FITS keyword and its value.
+
+    All keywords in the `keylist` file are added to all of the files
+    in `file_list`.
+
+    A sample `keylist` file is:
+        Keyword   Value
+        OBJCTDEC '+49 49 14'
+        OBJCTRA '09 02 21'
+
+    """
+    files =at.read(file_list)
+    key_table = at.read(keylist)
+    for fil in files:
+        fil_fits = pyfits.open(fil[0],mode='update')
+        hdr = fil_fits[0].header
+        for key, val in key_table:
+            print key, val
+            hdr.update(key, val)
+        fil_fits.close()
