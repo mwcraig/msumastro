89c2f16978a211a982e9aaa33d7bd8940e7fe4f7
Matt Craig <mcraig@mnstate.edu>
2012-03-28 15:39:02 -0500

    Display name of directory being processed, add object information.

--- run_patch.py
+++ run_patch.py
@@ -1,5 +1,8 @@
-from patch_headers import patch_headers
+from patch_headers import patch_headers, add_object_info
 import sys
 
 for currentDir in sys.argv[1:]:
+    print "working on directory: %s" % currentDir
     patch_headers(currentDir, overwrite=True)
+    add_object_info(currentDir)
+    
