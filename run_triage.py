b060852c888bea8ba2218b3aea9067c298e24348
Matt Craig <mcraig@mnstate.edu>
2012-03-28 16:48:17 -0500

    Make run_triage usable as either a module or a script.

--- run_triage.py
+++ run_triage.py
@@ -14,22 +14,26 @@ def write_list(dir, file, info):
     out.write('\n'.join(info))
     out.close()
 
-for currentDir in sys.argv[1:]:
+def triage_directories(directories,
+                       extra_keywords=[]):
+    for currentDir in directories:
 #    pdb.set_trace()
-    moo = tff.triage_fits_files(currentDir,
-                                file_info_to_keep=['imagetyp',
-                                                   'filter',
-                                                   'exptime',
-                                                   'ccd-temp'])
-    need_pointing = moo['needs_pointing']
-    if need_pointing:
-        write_list(currentDir, pointing_file_name, need_pointing)
-    if moo['needs_filter']:
-        write_list(currentDir, filter_file_name, moo['needs_filter'])
-    if moo['needs_object_name']:
-        write_list(currentDir, object_name_file_name,
-                   moo['needs_object_name'])
-    tbl = moo['files']
-    tbl.write(os.path.join(currentDir, file_list), type='ascii', delimiter=',')
+        moo = tff.triage_fits_files(currentDir,
+                                    file_info_to_keep=['imagetyp',
+                                                       'filter',
+                                                       'exptime',
+                                                       'ccd-temp']+extra_keywords)
+        need_pointing = moo['needs_pointing']
+        if need_pointing:
+            write_list(currentDir, pointing_file_name, need_pointing)
+        if moo['needs_filter']:
+            write_list(currentDir, filter_file_name, moo['needs_filter'])
+        if moo['needs_object_name']:
+            write_list(currentDir, object_name_file_name,
+                       moo['needs_object_name'])
+        tbl = moo['files']
+        tbl.write(os.path.join(currentDir, file_list), type='ascii', delimiter=',')
                  
-                     
+if __name__ == "__main__":
+    dirs = sys.argv[1:]
+    triage_directories(dirs, extra_keywords=['object'])
