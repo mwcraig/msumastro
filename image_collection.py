mwcraig/msumastro

commit e076b1929c11288ca86267ef5c40e591eaf8958e

Change default handling of **kwd in @iterate_files.

image_collection.py
@@ -399,7 +399,8 @@ class ImageFileCollection(object):
     @iterate_files
     def headers(self, save_with_name='',
                 save_location='', clobber=False,
-                hdulist=None, **kwd):
+                hdulist=None, do_not_scale_image_data=True,
+                **kwd):
         """
         Generator for headers in the collection including writing of
         FITS file before moving to next item.
@@ -421,6 +422,10 @@ class ImageFileCollection(object):
         clobber : bool
             If True, overwrite input FITS files.
 
+        do_not_scale_image_data : bool
+            If true, prevents pyfits from scaling images (useful for
+            preserving unsigned int images unmodified)
+        
         **kwd : dict
             Any additional keywords are passed to `pyfits.open`
         """

commit f70b3ead4fb3c6b427a428e18fd3b3066ba04e03

Fix regression introduced in e076b19 that caused corruption of scaled data.

image_collection.py
@@ -78,9 +78,12 @@ from tempfile import TemporaryFile
 def iterate_files(func):
     @functools.wraps(func)
     def wrapper(self, save_with_name="", save_location='',
-                clobber=False, hdulist=None, **kwd):
+                clobber=False, hdulist=None,
+                do_not_scale_image_data=True,
+                **kwd):
         for full_path in self.paths():
-            hdulist = pyfits.open(full_path, **kwd)
+            hdulist = pyfits.open(full_path,
+                                  do_not_scale_image_data=do_not_scale_image_data)
             yield func(self, save_with_name=save_with_name,
                        save_location='', clobber=clobber, hdulist=hdulist)
             if save_location: