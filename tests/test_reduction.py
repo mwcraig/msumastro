47c3779534add2b90fbd408c8caa902f3c54fb4f
Matt Craig <mattwcraig@gmail.com>
2012-09-01 13:03:05 -0500

    Add tests of master frame generation.

--- tests/test_reduction.py
+++ tests/test_reduction.py
@@ -2,6 +2,8 @@ from ..image_collection import ImageFileCollection
 import numpy as np
 from tempfile import mkdtemp
 test_dir = ''
+from ..master_bias_dark import master_bias_dark
+from ..master_flat import master_flat
     
 def setup():
     global test_dir
@@ -14,4 +16,39 @@ def teardown():
     from shutil import rmtree
 
     rmtree(test_dir)
-    
+
+def test_bias():
+    coll = ImageFileCollection(test_dir, keywords = ['imagetyp'])
+    all_data = []
+    for hdu in coll.hdus(imagetyp='bias'):
+        all_data.append(hdu.data)
+    all_data = np.array(all_data)
+    admean = np.mean(all_data, axis = 0)
+    admed = np.median(all_data, axis = 0)
+    mbd = master_bias_dark([test_dir], type = 'bias')
+    assert(mbd == admed)
+    assert(mbd != admean)
+
+def test_dark():
+    coll = ImageFileCollection(test_dir, keywords = ['imagetyp', 'exptime'])
+    all_data = []
+    for hdu in coll.hdus(imagetyp='dark'):
+        all_data.append(hdu.data)
+    all_data = np.array(all_data)
+    admean = np.mean(all_data, axis = 0)
+    admed = np.median(all_data, axis = 0)
+    mbd = master_bias_dark([test_dir], type = 'dark')
+    assert(mbd == admed)
+    assert(mbd != admean)
+
+def test_flat():
+    coll = ImageFileCollection(test_dir, keywords = ['imagetyp', 'exptime', 'filter'])
+    all_data = []
+    for hdu in coll.hdus(imagetyp='flat'):
+        all_data.append(hdu.data)
+    all_data = np.array(all_data)
+    admean = np.mean(all_data, axis = 0)
+    admed = np.median(all_data, axis = 0)
+    mf = master_flat.master_flat(test_dir)
+    assert(mf == admed)
+    assert(mf != admean)
