a8317f349d7a76f69035950e66a1796cecbe35a5
Matthew Craig <mattwcraig@gmail.com>
2013-03-24 15:55:58 -0500

    Propagate change in add_overscan to reduction tests

--- tests/test_reduction.py
+++ tests/test_reduction.py
@@ -1,6 +1,6 @@
 from ..image_collection import ImageFileCollection
 from ..reduction import trim
-from ..patch_headers import add_overscan
+from ..patch_headers import patch_headers
 import numpy as np
 import pyfits
 from tempfile import mkdtemp
@@ -16,12 +16,15 @@ def setup():
 
 
 def test_trim():
+    global test_dir
     from utilities import make_overscan_test_files
     from ..feder import ApogeeAltaU9
 
     oscan_dir, has_oscan, has_no_oscan = make_overscan_test_files(test_dir)
     chdir(path.join(test_dir, oscan_dir))
-    add_overscan('.', new_file_ext='', overwrite=True)
+    patch_headers('.', new_file_ext='', overwrite=True,
+                  add_time=False, purge_bad=False, add_apparent_pos=False,
+                  add_overscan=True)
     apogee = ApogeeAltaU9()
     # files without overscan should not be changed by trim
     hdus = pyfits.open(has_no_oscan)
