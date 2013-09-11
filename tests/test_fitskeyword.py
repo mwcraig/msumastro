e2ce41217a6c6445879b194089336c466de11a90
Matthew Craig <mattwcraig@gmail.com>
2013-09-11 11:21:58 -0500

    Fix test_fitskeyword to work with py.test
    Turns out the tests were not getting collected because the test class wasn't subclassing from object.

--- tests/test_fitskeyword.py
+++ tests/test_fitskeyword.py
@@ -1,60 +1,65 @@
 from ..fitskeyword import FITSKeyword
 from pyfits.hdu import PrimaryHDU
 
-class TestGoodFITSKeyword(FITSKeyword):
+
+class TestGoodFITSKeyword(object):
+    def setup_method(self, method):
+        self.keyword = FITSKeyword(name="kwd", value=12,
+                                   comment='This is a comment',
+                                   synonyms=['kwdalt1', 'kwdalt2'])
+        self.hdu = PrimaryHDU()
+
     def setUp(self):
         self.name = "kwd"
         self.value = 12
         self.comment = 'This is a comment'
         self.synonyms = ['kwdalt1', 'kwdalt2']
         self.hdu = PrimaryHDU()
-        
+
     def test_name_setter(self):
-        assert self.name == "KWD"
+        assert self.keyword.name == "KWD"
 
     def test_synonym_setter_length(self):
-        assert len(self.synonyms) == 2
+        assert len(self.keyword.synonyms) == 2
 
     def test_synonym_setter_values(self):
-        assert ((self.synonyms[0] == 'KWDALT1') and
-                (self.synonyms[1] == 'KWDALT2'))
+        assert ((self.keyword.synonyms[0] == 'KWDALT1') and
+                (self.keyword.synonyms[1] == 'KWDALT2'))
 
     def test_names(self):
-        assert self.names == [self.name, self.synonyms[0], self.synonyms[1]]
+        assert (self.keyword.names == [self.keyword.name,
+                self.keyword.synonyms[0], self.keyword.synonyms[1]])
 
     def test_history_comment(self):
-        assert self.historyComment() == "Updated keyword KWD to value 12"
-        assert self.historyComment(with_name=self.synonyms[0]) ==\
-            "Updated keyword KWDALT1 to value 12"
+        assert (self.keyword.historyComment() ==
+                "Updated keyword KWD to value 12")
+        assert (self.keyword.historyComment(with_name=self.keyword.synonyms[0])
+                == "Updated keyword KWDALT1 to value 12")
 
     def test_add_header(self):
-        self.addToHeader(self.hdu)
-        assert self.hdu.header[self.name] == self.value
-        for synonym in self.synonyms:
-            assert self.hdu.header[synonym] == self.value
-            
+        self.keyword.addToHeader(self.hdu)
+        assert self.hdu.header[self.keyword.name] == self.keyword.value
+        for synonym in self.keyword.synonyms:
+            assert self.hdu.header[synonym] == self.keyword.value
+
     def test_add_header_history(self):
-        self.addToHeader(self.hdu, history=True)
+        self.keyword.addToHeader(self.hdu, history=True)
         print self.hdu.header.get_history()
-        assert len(self.hdu.header.get_history()) == (1+len(self.synonyms))
+        assert (len(self.hdu.header.get_history()) ==
+                (1+len(self.keyword.synonyms)))
 
     def test_add_header_no_synonyms(self):
         clean_hdu = PrimaryHDU()
-        self.addToHeader(clean_hdu, with_synonyms=False)
-        for synonym in self.synonyms:
+        self.keyword.addToHeader(clean_hdu, with_synonyms=False)
+        for synonym in self.keyword.synonyms:
             try:
                 clean_hdu.header[synonym]
                 assert False
             except KeyError:
                 assert True
-                
+
     def test_add_header_from_header(self):
         clean_hdu = PrimaryHDU()
-        self.addToHeader(clean_hdu.header)
-        for name in self.names:
-            assert clean_hdu.header[name] == self.value
-    
-
-
-            
-
+        self.keyword.addToHeader(clean_hdu.header)
+        for name in self.keyword.names:
+            assert clean_hdu.header[name] == self.keyword.value
