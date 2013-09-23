573bf086a09da16925fe01c3016161a478e5a2af
Matthew Craig <mattwcraig@gmail.com>
2013-09-23 11:11:12 -0500

    Make copy of reference image with same name modification as the rest of the images

--- shifter.py
+++ shifter.py
@@ -10,6 +10,8 @@ def shift_images(files, source_dir, output_file='_shifted'):
     ref_im = ImageWithWCS(path.join(source_dir, ref))
     ref_pix = np.int32(np.array(ref_im.data.shape) / 2)
     ref_ra_dec = ref_im.wcs_pix2sky(ref_pix)
+    base, ext = path.splitext(files[0])
+    ref_im.save(path.join(source_dir, base + output_file + ext))
     for fil in files[1:]:
         img = ImageWithWCS(path.join(source_dir, fil))
         ra_dec_pix = img.wcs_sky2pix(ref_ra_dec)
