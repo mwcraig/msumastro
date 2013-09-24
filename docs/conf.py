72a331f24028549ac2e6c6e7a62cdfc339314cee
Matthew Craig <mattwcraig@gmail.com>
2013-09-24 09:36:08 -0500

    Add Sphinx argparse extension

--- docs/conf.py
+++ docs/conf.py
@@ -27,6 +27,7 @@ sys.path.insert(1, os.path.abspath('..'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.viewcode']
+extensions += ['sphinxarg.ext']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
