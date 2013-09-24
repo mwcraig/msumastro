2f466b59366383d58dee68dd79a59b5c88c05661
Matthew Craig <mattwcraig@gmail.com>
2013-09-23 23:24:44 -0500

    Edit documentation to reduce repetition
    In particular, discussion of command line options has been removed from the module doctoring because it is auto generated by argparse.

--- run_patch.py
+++ run_patch.py
@@ -1,11 +1,8 @@
 """
-SYNOPSIS
-
-    python run_patch.py dir1 [dir2 dir3 ...]
-
 DESCRIPTION
+-----------
 
-    For each directory dir1, dir2, ... provided on the command line the
+    For each directory provided on the command line the
     headers all of the FITS files in that directory are modified
     to add information like LST, apparent object position, and more.
     See the full documentation for a list of the specific keywords
@@ -33,6 +30,7 @@ DESCRIPTION
         specified on the command line.
 
 EXAMPLES
+--------
 
     Invoking this script from the command line::
 
@@ -56,16 +54,21 @@ def patch_directories(directories, verbose=False):
         patch_headers(currentDir, new_file_ext='', overwrite=True)
         add_object_info(currentDir, new_file_ext='', overwrite=True)
 
-if __name__ == "__main__":
+
+def construct_parser():
     import script_helpers
     import argparse
 
     parser = argparse.ArgumentParser()
     script_helpers.setup_parser_help(parser, __doc__)
     script_helpers.add_verbose(parser)
+    script_helpers.add_directories(parser)
 
-    parser.add_argument("directories", metavar='dir', nargs='+')
+    return parser
+
+if __name__ == "__main__":
+    parser = construct_parser()
     parser.parse_args()
-
     args = parser.parse_args()
-    patch_directories(args.directories, verbose=args.verbose)
+
+    patch_directories(args.dir, verbose=args.verbose)
