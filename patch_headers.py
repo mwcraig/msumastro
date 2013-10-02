08e2bc115367fbb37e09ef0a6c23cdd14020955a
Matthew Craig <mattwcraig@gmail.com>
2013-10-02 12:51:17 -0500

    Actually eliminate glob (*) import in patch_headers

--- patch_headers.py
+++ patch_headers.py
@@ -7,7 +7,7 @@ import astropy.io.fits as fits
 from astropysics import coords
 from astropy.time import Time
 
-from feder import *
+from feder import Feder
 
 from image_collection import ImageFileCollection
 
