6c9705dc5d0e87138aa44d09cef2267f6659165f
Matthew Craig <mattwcraig@gmail.com>
2014-03-03 15:16:38 -0600

    Fix line length

--- msumastro/tests/test_image_collection.py
+++ msumastro/tests/test_image_collection.py
@@ -399,7 +399,8 @@ class TestImageFileCollection(object):
         # Does ImageFileCollection summary contain values from table?
         assert nonsense in ic.summary_info['imagetyp']
 
-    def test_initializing_from_table_file_that_does_not_exist(self, triage_setup,
+    def test_initializing_from_table_file_that_does_not_exist(self,
+                                                              triage_setup,
                                                               caplog):
         # Do we get a warning if we try reading a file that doesn't exist,
         # but where we can initialize from a directory?
@@ -409,7 +410,8 @@ class TestImageFileCollection(object):
                     if ((rec.levelno == logging.WARN) &
                         ('Unable to open table file' in rec.message))]
         assert (len(warnings) == 1)
-        # Do we raise an error if the table name is bad AND the location is None?
+        # Do we raise an error if the table name is bad AND the location
+        # is None?
         with pytest.raises(IOError):
             ic = tff.ImageFileCollection(location=None,
                                          info_file='iufadsdhfasdifre')
