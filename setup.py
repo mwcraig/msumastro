715ae88d250a4f5b86a49ae2bc9ea3e5764dcfa2
Matthew Craig <mattwcraig@gmail.com>
2015-03-18 23:08:06 -0500

    Bump astropy and ccdproc version requirements

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ class PyTest(TestCommand):
         errcode = pytest.main(self.test_args)
         sys.exit(errcode)
 
-INSTALL_REQUIRES = ['astropy>=0.4.2', 'numpy', 'ccdproc>=0.2.2']
+INSTALL_REQUIRES = ['astropy>=1.0', 'numpy', 'ccdproc>=0.3']
 
 setup(
     name='msumastro',
