94cacd39ef58cdd652fc4a3d89bae1f3d60794b2
Matthew Craig <mattwcraig@gmail.com>
2015-04-23 14:52:40 +0200

    Bump version: 0.7.2 → 0.8.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ INSTALL_REQUIRES = ['astropy>=1.0', 'numpy', 'ccdproc>=0.3']
 
 setup(
     name='msumastro',
-    version='0.7.2',
+    version='0.8.0',
     description='Process FITS files',
     url='http://github.com/mwcraig/msumastro',
     long_description=(open('README.rst').read()),
