c962943fea456e9332bd72ff0570ccf48fd9cc87
Matthew Craig <mattwcraig@gmail.com>
2015-09-04 17:04:37 -0500

    Fix update from IERS Table A, if needed

--- msumastro/header_processing/patchers.py
+++ msumastro/header_processing/patchers.py
@@ -63,8 +63,8 @@ def _lst_from_obstime(obstime):
         # so get a newer one.
         from astropy.utils import iers
         from astropy.utils.data import download_file
-        iers.IERS.iers_table = iers.IERS_A.open(download_file(iers.IERS_A_URL,
-                                                cache=True))
+        iers_a = iers.IERS_A.open(download_file(iers.IERS_A_URL))
+        obstime.delta_ut1_utc = obstime.get_delta_ut1_utc(iers_a)
         LST = obstime.sidereal_time('apparent',
                                     longitude=feder.site.longitude)
     return LST
