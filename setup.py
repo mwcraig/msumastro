1599d4b092d095f6b62b1e6fec637e7867d6249a
Matt Craig <mattwcraig@gmail.com>
2022-11-26 13:20:02 -0600

    Modernize setup

--- setup.py
+++ setup.py
@@ -1,61 +1,50 @@
-from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
+#!/usr/bin/env python
+
 import sys
+from setuptools import setup
 
-import versioneer
+TEST_HELP = """
+Note: running tests is no longer done using 'python setup.py test'. Instead
+you will need to run:
 
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
+    tox -e test
 
-    def run_tests(self):
-        import pytest
-        errcode = pytest.main(self.test_args)
-        sys.exit(errcode)
+If you don't already have tox installed, you can install it with:
 
-INSTALL_REQUIRES = ['astropy>=3.2.2', 'numpy', 'ccdproc>=1.0']
+    pip install tox
 
-versioneer_cmdclass = versioneer.get_cmdclass()
-versioneer_cmdclass['test'] = PyTest
-setup(
-    name='msumastro',
-    version=versioneer.get_version(),
-    description='Process FITS files',
-    url='http://github.com/mwcraig/msumastro',
-    long_description=(open('README.rst').read()),
-    license='BSD 3-clause',
-    author='Matt Craig',
-    author_email='mcraig@mnstate.edu',
-    packages=find_packages(exclude=['tests*']),
-    include_package_data=True,
-    install_requires=INSTALL_REQUIRES,
-    extras_require={
-        'docs': ['numpydoc', 'sphinx-argparse', 'sphinx_rtd_theme', 'sphinx-astropy'],
-    },
-    tests_require=['scipy', 'pytest>=2.9'] + INSTALL_REQUIRES,
-    cmdclass=versioneer_cmdclass,
-    entry_points={
-        'console_scripts': [
-            ('quick_add_keys_to_file.py = '
-             'msumastro.scripts.quick_add_keys_to_file:main'),
-            ('run_patch.py = '
-             'msumastro.scripts.run_patch:main'),
-            ('run_astrometry.py = '
-             'msumastro.scripts.run_astrometry:main'),
-            ('run_triage.py = '
-             'msumastro.scripts.run_triage:main'),
-            ('run_standard_header_process.py = '
-             'msumastro.scripts.run_standard_header_process:main'),
-            ('sort_files.py = '
-             'msumastro.scripts.sort_files:main')
-        ]
-    },
-    classifiers=['Development Status :: 4 - Beta',
-                 'License :: OSI Approved :: BSD License',
-                 'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 2 :: Only',
-                 'Intended Audience :: Science/Research',
-                 'Topic :: Scientific/Engineering :: Astronomy'],
-    )
+If you only want to run part of the test suite, you can also use pytest
+directly with::
+
+    pip install -e .
+    pytest
+
+For more information, see:
+
+  http://docs.astropy.org/en/latest/development/testguide.html#running-tests
+"""
+
+if 'test' in sys.argv:
+    print(TEST_HELP)
+    sys.exit(1)
+
+DOCS_HELP = """
+Note: building the documentation is no longer done using
+'python setup.py build_docs'. Instead you will need to run:
+
+    tox -e build_docs
+
+If you don't already have tox installed, you can install it with:
+
+    pip install tox
+
+For more information, see:
+
+  http://docs.astropy.org/en/latest/install.html#builddocs
+"""
+
+if 'build_docs' in sys.argv or 'build_sphinx' in sys.argv:
+    print(DOCS_HELP)
+    sys.exit(1)
+
+setup(use_scm_version=True)
